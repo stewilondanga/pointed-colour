d99dcbd80872dea1bc1913bc037fa69736367db6
Stewart ilondanga <stilondanga@gmail.com>
2020-09-16 12:27:12 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,34 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+$('input').on('input',function(){
+    var value = $(this).attr('id');
+    $('table').css('--'+value,$(this).val()+$(this).data('unit'));
+    $(this).next('output').text($(this).val()+$(this).data('unit'));
+});
+
+$('td').on('mouseenter mouseleave',function(){
+    var i = $(this).index();
+    $(this).toggleClass('hover');
+    $(this).prev().toggleClass('hover');
+    $(this).next().toggleClass('hover');
+    $(this).parent().prev().find('td:eq('+i+')').toggleClass('hover');
+    $(this).parent().next().find('td:eq('+i+')').toggleClass('hover');
+});
+
+$('table').bind('touchmove', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    $('td').removeClass('hover');
+    var $this = $(document.elementFromPoint(event.touches[0].clientX, event.touches[0].clientY));
+    var i = $this.index();
+    $this.addClass('hover');
+    $this.prev().addClass('hover');
+    $this.next().addClass('hover');
+    $this.parent().prev().find('td:eq('+i+')').addClass('hover');
+    $this.parent().next().find('td:eq('+i+')').addClass('hover');
+});
+$('table').bind('touchend', function(event) {
+    $('td').removeClass('hover');
+});
